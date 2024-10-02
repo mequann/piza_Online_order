495a2832fd3d42b7afddfcf22f3995220441b8c5
Mequannt <mekacham381@gmail.com>
2024-10-02 17:26:59 +0300

    package issue resolved

--- frontend/src/util/api.js
+++ frontend/src/util/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const base_url='https://piza-online-order.onrender.com'
+const base_url='http://127.0.0.1:4000/api/'
 
 export default axios.create({
     baseURL: base_url,
