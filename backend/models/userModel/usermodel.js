63ae786954f5f2a5cc729f6ee75237d2afdf1a8d
Mequannt <mekacham381@gmail.com>
2024-10-13 22:57:41 +0300

    bcrypt substituted by bcryptjs

--- backend/models/userModel/usermodel.js
+++ backend/models/userModel/usermodel.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const sequelize = require("../../config/db_config");
 const Role = require("../roleModel/roleM");
 
